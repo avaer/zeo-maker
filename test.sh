53da7635d00c0f713c3720e1227f0085d9d48026
Adrian Biedrzycki <a@modules.io>
2017-10-21 17:53:42 -0700

    Clean up test generation script

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
-node index.js ITEM.WOOD keys.json -q 100 | curl -F 'clbin=<-' https://clbin.com
+node index.js ITEM.CRD keys.json -q 100 | curl -F 'clbin=<-' https://clbin.com
+node index.js ITEM.SWORD keys.json -q 100 | curl -F 'clbin=<-' https://clbin.com
